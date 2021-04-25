7e1c636279a20631eef5b744dcf2cc2751447041
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-04-25 11:12:19 +0200

    Added exercise010

---
+++ exercises/Exercise010.java
@@ -0,0 +1,42 @@
+public class Exercise010 {
+
+    public static void main(String[] args) {
+        int limit = 2_000_000;
+        long sum = 0;
+        boolean[] sieve = primeSieve(limit);
+        for (int i = 1; i <= limit; i++)
+        {
+            if (sieve[i-1]) sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    /**
+     * This method returns the prime sieve of all numbers up to a certain limit;
+     * @param limit the limit of the prime sieve.
+     * @return a boolean[] representing the sieve up to limit.
+     */
+    public static boolean[] primeSieve(int limit)
+    {
+        if (limit <= 0) throw new IllegalArgumentException("Limit must be >= 1");
+
+        // limit >= 1 here
+        boolean[] sieve = new boolean[limit];
+        for (int i = 2; i <= limit; i++) sieve[i-1] = true;
+
+        for (int i = 2; i <= Math.sqrt(limit); i++)
+        {
+            if (sieve[i-1])
+            {
+                int j = i * i;
+                while (j <= limit)
+                {
+                    sieve[j-1] = false;
+                    j += i;
+                }
+            }
+        }
+
+        return sieve;
+    }
+}
