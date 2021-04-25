4576f8c6c5cf9a58c1468613f399f9774c243309
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-04-25 10:38:49 +0200

    Added exercise 005 and 006

---
+++ exercises/Exercise005.java
@@ -0,0 +1,7 @@
+public class Exercise005 {
+
+    public static void main(String[] args) {
+        long n = (long) (Math.pow(2,4) * Math.pow(3,2) * 5 * 7 * 11 * 13 * 17 * 19);
+        System.out.println(n);
+    }
+}
