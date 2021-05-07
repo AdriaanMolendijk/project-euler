2608008f687883e98167aa7c23dd03fadfadd096
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-07 17:46:43 +0200

    Moved Coord class into exercise classes

--- exercises/Exercise091.java
+++ exercises/Exercise091.java
@@ -50,4 +50,44 @@ public class Exercise091 {
         return Math.sqrt(distX * distX + distY * distY);
     }
 
+    public static class Coord {
+
+        private int x;
+        private int y;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public boolean equals(Coord o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coord coord = (Coord) o;
+            return x == coord.x && y == coord.y;
+        }
+
+        public String toString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+    }
+
 }
--- exercises/Exercise102.java
+++ exercises/Exercise102.java
@@ -35,4 +35,44 @@ public class Exercise102 {
     {
         return Math.abs((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / 2.0);
     }
+
+    private static class Coord {
+
+        private int x;
+        private int y;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public boolean equals(Coord o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coord coord = (Coord) o;
+            return x == coord.x && y == coord.y;
+        }
+
+        public String toString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+    }
 }
