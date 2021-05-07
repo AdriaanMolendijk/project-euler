40637759b709ff4bfa5a0ffd77ce97b1420996a4
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-07 10:23:35 +0200

    Added 187

---
+++ exercises/Exercise187.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Exercise187 {
+
+    public static void main(String[] args) {
+        int limit = 100_000_000;
+        boolean[] sieve = primeSieve(limit / 2);
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for (int i = 1; i <= limit / 2; i++)
+        {
+            if (sieve[i - 1])
+            {
+                primes.add(i);
+            }
+        }
+        int n = primes.size();
+
+        HashSet<Long> composites = new HashSet<>();
+        long p, q, composite;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                p = (long) primes.get(i);
+                q = (long) primes.get(j);
+                composite = p * q;
+                if (composite > limit) break;
+                composites.add(composite);
+            }
+        }
+        System.out.println(composites.size());
+    }
+
+    /**
+     * This method returns the prime sieve of all numbers up to a certain limit;
+     * @param limit the limit of the prime sieve.
+     * @return a boolean[] representing the sieve up to limit.
+     */
+    public static boolean[] primeSieve(int limit)
+    {
+        if (limit <= 0) throw new IllegalArgumentException("Limit must be >= 1");
+
+        // limit >= 1 here
+        boolean[] sieve = new boolean[limit];
+        for (int i = 2; i <= limit; i++) sieve[i-1] = true;
+
+        for (int i = 2; i <= Math.sqrt(limit); i++)
+        {
+            if (sieve[i-1])
+            {
+                int j = i * i;
+                while (j <= limit)
+                {
+                    sieve[j-1] = false;
+                    j += i;
+                }
+            }
+        }
+        return sieve;
+    }
+
+}
