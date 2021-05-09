03f0bcb242ce912afc1eb0dea270559e550b86d8
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-09 10:02:49 +0200

    Added python ex 67

---
+++ python/exercise067.py
@@ -0,0 +1,11 @@
+size = 100
+with open('exercise067.txt', 'r') as f:
+    grid = [[int(j) for j in i.split(' ')] for i in f]
+path = [[0 for i in range(size)] for j in range(size)]
+for col in range(size):
+    path[size-1][col] = grid[size-1][col]
+for i in range(size - 2, -1, -1):
+    for j in range(0, i + 1):
+        path[i][j] = grid[i][j] + max(path[i+1][j], path[i+1][j+1])
+print(path[0][0])
+print(path)
